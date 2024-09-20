40f928d20e46f0b36d2a6cc147a2e49f829466e3
shwn <zxz8673@gmail.com>
2024-09-20 23:31:37 +0900

    [add] Swagger 꾸미기

--- 03_SpringBoot/week2/src/main/java/com/pironeer/week2/controller/TopicController.java
+++ 03_SpringBoot/week2/src/main/java/com/pironeer/week2/controller/TopicController.java
@@ -5,6 +5,8 @@ import com.pironeer.week2.dto.request.TopicUpdateRequest;
 import com.pironeer.week2.dto.response.TopicResponse;
 import com.pironeer.week2.repository.domain.Topic;
 import com.pironeer.week2.service.TopicService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,35 +15,41 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor // 생성자를 annotation으로 만들 수 있다.
+@Tag(name = "게시물(Topic)")
 @RequestMapping("/api/topic") // 으로 시작하도록
 public class TopicController {
     private final TopicService topicService;
 
     @PostMapping// Post 요청 받음
+    @Operation(summary = "게시물 작성")
     public ResponseEntity<?> create(@RequestBody TopicCreateRequest request) { // RequestBody가 request변수에 값을 담음
         topicService.save(request);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping("/{topicId}")
+    @Operation(summary = "게시물 단건 조회")
     public ResponseEntity<TopicResponse> read(@PathVariable("topicId") Long id) { //변수를 가져오는 annotation
         TopicResponse response = topicService.findById(id);
         return ResponseEntity.ok().body(response);
     }
 
     @GetMapping
+    @Operation(summary = "게시물 전체 조회")
     public ResponseEntity<List<TopicResponse>> readAll() {
         List<TopicResponse> responses = topicService.findAll();
         return ResponseEntity.ok().body(responses);
     }
 
     @PatchMapping
+    @Operation(summary = "게시물 수정")
     public ResponseEntity<TopicResponse> update(@RequestBody TopicUpdateRequest request) {
         TopicResponse response = topicService.update(request);
         return ResponseEntity.ok().body(response);
     }
 
     @DeleteMapping
+    @Operation(summary = "게시물 삭제")
     public ResponseEntity<?> remove(@PathVariable("topicId") Long id) {
         topicService.deleteById(id);
         return ResponseEntity.ok().build();
